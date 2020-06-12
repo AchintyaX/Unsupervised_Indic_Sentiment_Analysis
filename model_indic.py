77331183a476dc88a918529f2acc0a6de7c533dc
AchintyaX <achintyashankhdhar@gmail.com>
2020-06-12 21:39:49 +0530

    new version of the get_polarity

---
+++ model_indic.py
@@ -0,0 +1,54 @@
+from gensim.models.fasttext import FastText
+
+
+# Getting the polarity of each word 
+def get_polarity(word, word_list, model):
+    num_words = len(word_list)
+    score = 0
+    for i in word_list:
+        try:
+            similarity = model.wv.similarity(word, i['word']) 
+        except KeyError:
+            similarity = 0
+        if i['pos']>i['neg']:
+            coeff = i['pos']
+        else:
+            coeff = i['neg']
+        score = score + (similarity*coeff)
+    score = score/num_words 
+
+    return score 
+
+# Getting the sentiment vectors of each sentence 
+
+def get_senti_coeff_indic(article, pos_words, neg_words, model):
+    sentiment_vector = []
+    for word in article:
+        token_pos = get_polarity(word, pos_words, model)
+        token_neg = get_polarity(word, neg_words, model)
+        if token_pos > token_neg:
+            token = token_pos
+        elif token_pos == token_neg:
+            token = 0.00
+        else:
+            token = -1*token_neg
+        sentiment_vector.append(token)
+    return sentiment_vector
+
+# updated version of the  get polarity function
+def get_sentiment(word, word_list, model):
+    max_similarity = 0
+    max_score = 0
+    for i in word_list:
+        try:
+            similarity = model.wv.similarity(word, i['word'])
+        except:
+            similarity = 0 
+        if i['pos']> i['neg']:
+            coeff = i['pos']
+        else:
+            coeff = i['neg']
+        score = similarity*coeff
+        if score > max_similarity:
+            max_similarity = score
+    return  max_similarity
