6bba24775f88c65680e697dc6ac847c3327681b1
AchintyaX <achintyashankhdhar@gmail.com>
2020-06-26 22:59:18 +0530

    updates

--- model_indic.py
+++ model_indic.py
@@ -24,25 +24,26 @@ def get_polarity(word, word_list, model):
 def get_senti_coeff_indic(article, pos_words, neg_words, model):
     sentiment_vector = []
     for word in article:
-        token_pos = get_polarity(word, pos_words, model)
-        token_neg = get_polarity(word, neg_words, model)
-        if token_pos > token_neg:
+        token_pos = get_sentiment(word, pos_words, model)
+        token_neg = get_sentiment(word, neg_words, model)
+        if token_pos >= token_neg:
             token = token_pos
-        elif token_pos == token_neg:
-            token = 0.00
+            if token < 0.40:
+                token = 0
         else:
             token = -1*token_neg
+            if token > -0.40:
+                token = 0 
         sentiment_vector.append(token)
     return sentiment_vector
 
 # updated version of the  get polarity function
 def get_sentiment(word, word_list, model):
     max_similarity = 0
-    max_score = 0
     for i in word_list:
         try:
             similarity = model.wv.similarity(word, i['word'])
-        except:
+        except KeyError:
             similarity = 0 
         if i['pos']> i['neg']:
             coeff = i['pos']
